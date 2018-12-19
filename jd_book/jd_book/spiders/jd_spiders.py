326c589c0e4b745229dcb9b1a3ff95e8f493be32
duxiaobo <1744381765@qq.com>
2018-12-19 18:21:22 +0800

    scrapy

---
+++ jd_book/jd_book/spiders/jd_spiders.py
@@ -0,0 +1,34 @@
+#coding: utf-8
+##用scrapy框架爬京东
+
+import scrapy
+import re
+from jd_book.items import JdBookItem
+
+#处理下获取到的url_str字符串，返回最终访问京东的合适的url
+def combine_url(url_str):
+    pattern = re.compile("\d+-\d+-\d+")
+    cat_resoure = pattern.findall(url_str)[0]
+    cat_list = cat_resoure.split("-")
+    cat = ",".join(cat_list)
+    tid = cat_list[-1]
+    url = "%s?cat=%s&tid=%s" %('https://list.jd.com/list.html',cat,tid)
+    return url
+
+class JdSpider(scrapy.Spider):
+        name = 'jd_home'
+        allowed_domains = ['jd.com']
+        start_urls = [
+            "https://book.jd.com/booksort.html"
+        ]
+        def parse(self,response):
+                for sel in response.xpath('//*[@id="booksort"]/div[@class="mc"]/dl/dd/em'):
+                        item = JdBookItem()
+                        item['nav2_name'] = sel.xpath('a/text()').extract()[0]
+                        relate_url = sel.xpath('a/@href').extract()[0]
+                        nav2_url = combine_url(relate_url)
+                        item['nav2_url'] = nav2_url
+                        yield scrapy.Request(url,callback=self.book_parse,meta={'item':item})
+        def book_parse(self,reponse):
+                item =
+
